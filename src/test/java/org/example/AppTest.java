aeb1e6591e5ab68a556bc198b7799c4ec9fa577d
revan <revanth2023ks@gmail.com>
2025-06-08 21:05:10 +0530

    first commit

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,35 @@
+package org.example;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertTrue;
+
+public class AppTest {
+    private static WebDriver driver;
+
+    @BeforeTest
+    public void openBrowser() throws InterruptedException {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+        driver.get("https://trisam3118.github.io/demowebsite/");// here enter static website URL from your GITHUB
+    }
+
+    @Test
+    public void titleValidationTest(){
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Computer Science and Engineering(Data Science)";
+        Assert.assertEquals(actualTitle, expectedTitle);
+        assertTrue(true, "Title should contain 'Computer Science and Engineering(Data Science)'");
+    }
+
+    @AfterTest
+    public void closeBrowser() throws InterruptedException {
+        Thread.sleep(1000);
+        driver.quit();
+    }
+
+}
